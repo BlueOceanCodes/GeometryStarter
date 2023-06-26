e3f3c9ec398589a4f9b3dd95f87dd2879600faf4
Alex Hudson <AHudson@tql.com>
2023-06-26 18:25:41 -0400

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Alex Hudson on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
